2699cb10a52a9443147f03ce2bc1f8d75ef87fd1
dmssantos <dms.santos_@hotmail.com>
2021-12-08 15:25:51 -0300

    created userSignin, userSignup and middleware

--- dio-inter-clone-backend/src/index.ts
+++ dio-inter-clone-backend/src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
+import { globalErrors } from './middleware/globalErros';
 import routes from './routes';
 
 const app = express();
 const PORT = 3333;
 
-app.use(routes)
+app.use(routes);
+
+app.use(globalErrors);
 
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
---
+++ dio-inter-clone-backend/src/resources/user/user.service.ts
@@ -0,0 +1,27 @@
+import { getRepository } from 'typeorm';
+import md5 from 'crypto-js/md5';
+import {User} from '../../entity/User';
+
+import {UserSignIn} from './dtos/user.signin.dtos';
+import {UserSignUp} from './dtos/user.signup.dtos';
+import AppError from '../../shared/error/AppError';
+export default class UserService {
+
+  async signin( user: UserSignIn){
+    const userRepository = getRepository(User);
+
+    const {email, password} = user;
+
+    const passwordHash = md5(password).toString();
+
+    const existUser = await userRepository.findOne({where: {email, password: passwordHash}});
+
+    if(!existUser) {
+      throw new AppError('Usuário não encontrado', 401);
+    };
+  }
+
+  async signup( user: UserSignUp){
+    
+  }
+}
