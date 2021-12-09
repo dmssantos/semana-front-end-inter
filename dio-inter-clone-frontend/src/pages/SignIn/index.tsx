5eeb5392e203d0f61d4abf7a55fcee6184e457e0
dmssantos <dms.santos_@hotmail.com>
2021-12-09 16:15:35 -0300

    Created signin page

--- dio-inter-clone-frontend/src/pages/SignIn/index.tsx
+++ dio-inter-clone-frontend/src/pages/SignIn/index.tsx
@@ -4,6 +4,9 @@ import Card from '../../components/Card';
 
 import background from '../../assets/images/background-login.jpg';
 import logoInter from '../../assets/images/Inter-orange.png';
+import Input from '../../components/Inputs';
+import Button from '../../components/Button';
+import { Link } from 'react-router-dom';
 
 const SignIn = () => {
     return (
@@ -11,6 +14,16 @@ const SignIn = () => {
             <Background image={background} />
             <Card width="403px">
                 <img src={logoInter} width={172} height={61} alt="logo inter" />
+
+                <InputContainer>
+                    <Input placeholder="EMAIL" />
+                    <Input placeholder="SENHA" type="password" />
+                </InputContainer>
+
+                <ButtonContainer>
+                    <Button type="button" >Entrar</Button>
+                    <p>Ainda não é cadastrado? <Link to="/signup">Cadastre-se já</Link></p>
+                </ButtonContainer>
             </Card>
         </Wrapper>
     )
--- dio-inter-clone-frontend/src/routes/index.tsx
+++ dio-inter-clone-frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import {
     Routes,
     Route,
   } from "react-router-dom";
+  
   import SignIn from '../pages/SignIn';
   import SignUp from '../pages/SignUp';
   import Dashboard from '../pages/Dashboard';
